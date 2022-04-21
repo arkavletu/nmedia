21798303b8cd088dacc1dc1044add6700b760ff8
Alina Prokofeva <whitestring@inbox.ru>
2022-04-21 16:20:49 +0300

    likes

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,5 +1,37 @@
 package ru.netology.nmedia
 
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+//import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(1,"Vova", "Let it crash", "21.04.2022")
+
+        binding.render(post)
+
+        binding.likes.setOnClickListener {
+            post.liked = !post.liked
+            binding.likes.setImageResource(getImageRes(post.liked))
+        }
+    }
+
+    private fun ActivityMainBinding.render(post: Post){
+        author.text = post.author
+        date.text = post.date
+        content.text = post.content
+        likes.setImageResource(getImageRes(post.liked))
+    }
+
+    @DrawableRes
+    private fun getImageRes(isLiked: Boolean) = if (isLiked) R.drawable.liked_24 else R.drawable.likes_24dp
+}
