e70a64751f57b3d896c3e20c95354ce5cde53022
Alina Prokofeva <whitestring@inbox.ru>
2022-05-20 14:14:16 +0300

    repost

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.ImageButton
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import kotlin.math.absoluteValue
 
 //import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity(){
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(1, "Vova", "Let it crash", "21.04.2022")
+        val post = Post(1, "Vova", "Let it crash", "21.04.2022",count_reposts = 999)
 
         binding.render(post)
 
@@ -25,9 +26,9 @@ class MainActivity : AppCompatActivity(){
             binding.countLikes.text = getSmthCounted(post, post.liked).toString()
         }
         binding.share.setOnClickListener {
-            post.shared = !post.shared
-            binding.countReposts.text = ( + post.count_reposts++).toString()
-        }//срабатывает со второго тача
+            post.shared = true
+            binding.countReposts.text = (post.count_reposts++).toString()
+        }//по первому клику сумма остается 0
 
     }
 
@@ -37,11 +38,19 @@ class MainActivity : AppCompatActivity(){
         content.text = post.content
         likes.setImageResource(getImageRes(post.liked))
         countLikes.text = getSmthCounted(post, post.liked).digitToChar().toString()
-        countReposts.text = post.count_reposts.toString()
+        countReposts.text = spellCounterOfRepo(post)
     }
 
     @DrawableRes
     private fun getImageRes(isLiked: Boolean) = if (isLiked) R.drawable.liked_24 else R.drawable.likes_24dp
     private fun getSmthCounted(post: Post,likedIs: Boolean): Int = if(likedIs) 1 else  0
-    //private fun getCountRepo(post: Post, isShared: Boolean) = if (isShared) +1 else post.count_reposts
+
+    private fun spellCounterOfRepo(post: Post):String{
+        return when{
+            post.count_reposts >= 1000000 -> "1M"
+            post.count_reposts >= 10000 -> "${(post.count_reposts/10000).absoluteValue}K"
+            post.count_reposts >= 1000 ->"${post.count_reposts/1000}K"
+            else -> post.count_reposts.toString()
+        }
+    }
 }
