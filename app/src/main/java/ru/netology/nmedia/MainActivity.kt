431ad33a4663995575b940b64b3c22b3de672c71
Alina Prokofeva <whitestring@inbox.ru>
2022-06-12 20:25:28 +0300

    1999

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(){
 
             post.count_reposts >= thousand -> "${if(post.count_reposts % thousand > 0) (post.count_reposts/thousand).
             toBigDecimal().setScale(1, RoundingMode.DOWN).toDouble() else 
-                    (post.count_reposts/thousand).toInt()}M"//1.0 отбросить дробную часть
+                    (post.count_reposts/thousand).toInt()}K"//1.0 отбросить дробную часть
 
             else -> post.count_reposts.toString()
         }
