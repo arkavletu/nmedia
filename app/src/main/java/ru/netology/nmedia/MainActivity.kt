7e7a1a15bdb4417827c3affad002428ac4b8bfe6
Alina Prokofeva <whitestring@inbox.ru>
2022-06-12 16:27:17 +0300

    1199999

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity(){
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(1, "Vova", "Let it crash", "21.04.2022",count_reposts = 10999)
+        val post = Post(1, "Vova", "Let it crash", "21.04.2022",count_reposts = 1199999)
 
         binding.render(post)
 
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity(){
 
     private fun spellCounterOfRepo(post: Post):String{
         return when{
-            post.count_reposts >= 1000000 -> "1M"//1.1
+            post.count_reposts >= 1000000 -> "${post.count_reposts/1000000}M"//1.1
             post.count_reposts >= 10000 -> "${(post.count_reposts/1000).absoluteValue}K"
             post.count_reposts >= 1000 ->"${(post.count_reposts/1000)}K"//1.1
             else -> post.count_reposts.toString()
