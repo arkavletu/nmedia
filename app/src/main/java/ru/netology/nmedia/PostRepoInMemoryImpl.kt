e4dc1f38684ee14d6f262b6a97afa638a8ab2653
Alina Prokofeva <whitestring@inbox.ru>
2022-06-28 20:36:33 +0300

    mvvm

--- app/src/main/java/ru/netology/nmedia/PostRepoInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/PostRepoInMemoryImpl.kt
@@ -17,14 +17,12 @@ class PostRepoInMemoryImpl: PostRepo {
     override fun like() {
         val oldPost = checkNotNull(data.value){"no nullable"}
         val newPost = oldPost.copy(liked = !oldPost.liked)
-       // newPost.count_likes = getLikesCounted(newPost.liked)
         data.value = newPost
     }
     override fun share(){
         val oldPost = checkNotNull(data.value){"no nullable"}
-        val newPost = oldPost.copy(count_reposts = +1)
+        val newPost = oldPost.copy(count_reposts = oldPost.count_reposts++)
         data.value = newPost
     }
 
-    //private fun getLikesCounted(isLiked: Boolean): Int = if(isLiked) 1 else  0
 }
